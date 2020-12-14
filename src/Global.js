8badfd43ad75ef3605352e4755485ce30a05da9a
911harm <911harm@gmail.com>
2020-12-14 07:57:18 -0500

    req token

---
+++ src/Global.js
@@ -0,0 +1,3 @@
+export const API={
+    auth:"https://java.bocetos.co/userred-0.0.1-SNAPSHOT/auth"
+}
--- src/components/login.js
+++ src/components/login.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useState } from 'react'
+import { API } from '../Global'
+const { auth } = API
+export default function Login() {
+    const [user, setUser] = useState("")
+    const [pass, setPass] = useState("")
+    const [token, setToken] = useState("")
 
-export default function login() {
-    const [user, setUser] = useState({})
-    AuthSubmit=(e)=>{
-        e.prevent.default();
-        console.log("Enviado")
+    const authSubmit = (e) => {
+        e.preventDefault();
+        
+        Axios.post(auth,
+            {
+                username: user,
+                password: pass
+            })
+            .then(res =>{
+                setToken(res.data.Authorization)
+            })
+    }
+    const changeU = (e) => {
+        setUser(e.target.value)
+
+    }
+    const changeP = (e) => {
+        setPass(e.target.value)
     }
 
+    console.log(token)
     return (
         <div className="login">
-            <form className="login-form" onSubmit={AuthSubmit}>
-                <input className="input-user" type="text" placeholder="User"/>
-                <input className="input-pass" type="password" placeholder="Password"/>
-                <button className="btn input-btn" type="submit" >Sing in</button>
+            <form className="login-form" onSubmit={authSubmit}>
+                <img src="https://acf.geeknetic.es/imgw/imagenes/tutoriales/1375/1375-1.jpg?f=webp" alt="logo" />
+                <input className="input user" onChange={changeU} type="text" placeholder="User" />
+                <input className="input pass" onChange={changeP} type="password" placeholder="Password" />
+                <button className="btn input btn" type="submit" >Sing in</button>
             </form>
         </div>
     )
